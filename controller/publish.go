3bb045a769b6f9eb8c43456f82003367147a97c3
for shell <fromShell@paas.com>
2023-08-17 21:43:33 +0800

    v1

--- controller/publish.go
+++ controller/publish.go
@@ -1,59 +1,191 @@
 package controller
 
 import (
+	"bytes"
 	"fmt"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	// "io"
+	"os"
+
+   "github.com/RaymondCode/simple-demo/models"
+  // "time"
+	"github.com/disintegration/imaging"
+
 	"github.com/gin-gonic/gin"
+	"strings"
+
 	"net/http"
+  // "net"
+
 	"path/filepath"
 )
 
 type VideoListResponse struct {
 	Response
+
 	VideoList []Video `json:"video_list"`
 }
 
+// ----------------------------TO DO------------------
+// 抽取某一帧作为图片
+func Vedio2Jpeg(inFileName string, frameNum int) {
+	buf := bytes.NewBuffer(nil)
+
+	// 使用 ffmpeg 命令行工具提取视频的指定帧作为 JPEG 图像
+	err := ffmpeg.Input(inFileName).
+		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
+		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(buf, os.Stdout).
+		Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	img, err := imaging.Decode(buf)
+	if err != nil {
+		return
+	}
+
+	index := strings.LastIndex(inFileName, ".")
+	// var outFileName strings
+	if index != -1 {
+		outFileName := strings.Join([]string{inFileName[:index+1], "jpeg"}, "")
+    fmt.Print(outFileName)
+		err = imaging.Save(img, outFileName)
+		if err != nil {
+			return 
+		} else {
+			return
+		}
+	}
+}
+
+// 获得 域名 
+func getDomain(c *gin.Context) (string, error) {
+	domain := c.Request.Host
+  fmt.Println("My domain:", domain)
+	return domain, nil
+}
+
+// -------------------------------------------------------------------
 // Publish check token then save upload file to public directory
+
 func Publish(c *gin.Context) {
+
+  // mysql -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USER} -p
 	token := c.PostForm("token")
 
-	if _, exist := usersLoginInfo[token]; !exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
-		return
-	}
+	// 鉴权, var usersLoginInfo = map[string]User
+  user_name, err := ParseToken(token);
+	// if  err != nil {
 
+	// 	c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+
+	// 	return
+
+	// }
+
+  // var user_id int64;
+  user, err := models.NewUserDaoInstance().FindUserByName(user_name); 
+   if err != nil {
+        panic(err)
+    }
+  var user_id = user.UserId
+
+  
+  
+
+	//
 	data, err := c.FormFile("data")
+
 	if err != nil {
+
 		c.JSON(http.StatusOK, Response{
+
 			StatusCode: 1,
-			StatusMsg:  err.Error(),
+
+			StatusMsg: err.Error(),
 		})
+
 		return
+
 	}
 
+	// 本地存储文件
 	filename := filepath.Base(data.Filename)
-	user := usersLoginInfo[token]
-	finalName := fmt.Sprintf("%d_%s", user.Id, filename)
+
+	// user := usersLoginInfo[token]
+
+	finalName := fmt.Sprintf("%d_%s", user_id, filename)
+
 	saveFile := filepath.Join("./public/", finalName)
+
+  // 获得域名
+  domain :=  c.Request.Host
+  // My domain: 2f781ee3592dd7a9ff0bbd0007fe40ce-app.1024paas.com
+  // fmt.Println("My domain:", domain)
+
+  vedio_url := "https://"+ domain + "/static/"+ finalName
+  // index := "https://" + strings.LastIndex(finalName, ".")
+  
+  img_url := domain + "/static/" + strings.TrimSuffix(finalName, filepath.Ext(finalName)) + ".jpeg"
+  // fmt.Println("vedio:", vedio_url, "     ", img_url)
+  // vedio: https://2f781ee3592dd7a9ff0bbd0007fe40ce-app.1024paas.com/static/0_20230329_133339.mp4       2f781ee3592dd7a9ff0bbd0007fe40ce-app.1024paas.com/static/0_20230329_133339.jpeg
+  
+  
+
+  
+
+	// func (c *gin.Context) SaveUploadedFile(file *multipart.FileHeader, dst string) error
 	if err := c.SaveUploadedFile(data, saveFile); err != nil {
+    
+    
 		c.JSON(http.StatusOK, Response{
+
 			StatusCode: 1,
-			StatusMsg:  err.Error(),
+
+			StatusMsg: err.Error(),
 		})
+
 		return
+
+	}
+  Vedio2Jpeg(saveFile, 6)
+  
+
+	video1 := models.Video{
+		UserId : user_id,
+		PlayUrl : vedio_url,
+    CoverUrl : img_url , 
 	}
 
+  // put it to database
+  _, err = models.NewVideoDaoInstance().CreateVideo(&video1)
+
+
+  
 	c.JSON(http.StatusOK, Response{
+
 		StatusCode: 0,
-		StatusMsg:  finalName + " uploaded successfully",
+
+		StatusMsg: finalName + " uploaded successfully",
 	})
+
 }
 
 // PublishList all users have same publish video list
+
 func PublishList(c *gin.Context) {
+
 	c.JSON(http.StatusOK, VideoListResponse{
+
 		Response: Response{
+
 			StatusCode: 0,
 		},
+
 		VideoList: DemoVideos,
 	})
+
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,21 @@ module github.com/RaymondCode/simple-demo
 go 1.17
 
 require (
+	github.com/disintegration/imaging v1.6.2
 	github.com/gavv/httpexpect/v2 v2.8.0
 	github.com/gin-gonic/gin v1.7.7
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/stretchr/testify v1.7.0
+	github.com/u2takey/ffmpeg-go v0.5.0
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -18,14 +25,12 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/imkira/go-interpol v1.1.0 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -36,6 +41,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sanity-io/litter v1.5.5 // indirect
 	github.com/sergi/go-diff v1.0.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.34.0 // indirect
@@ -45,12 +51,11 @@ require (
 	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
 	github.com/yudai/gojsondiff v1.0.0 // indirect
 	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
-	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	moul.io/http2curl/v2 v2.3.0 // indirect
 )
